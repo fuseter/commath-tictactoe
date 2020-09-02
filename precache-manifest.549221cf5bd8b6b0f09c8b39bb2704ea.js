b7b20b9fcaed539e12b6ca86d50f906a58bc2d8f
fuseter <phongkhanet.ka.61@ubu.ac.th>
2020-09-02 10:24:34 +0700

    Updates

---
+++ precache-manifest.549221cf5bd8b6b0f09c8b39bb2704ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc4c9897b9f05206259e4fb9617d433",
+    "url": "/commath-tictactoe/index.html"
+  },
+  {
+    "revision": "ef389fc0cc4c88eadd20",
+    "url": "/commath-tictactoe/static/css/main.b4da59bc.chunk.css"
+  },
+  {
+    "revision": "d1534d28dd47e618c07e",
+    "url": "/commath-tictactoe/static/js/2.0252234b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/commath-tictactoe/static/js/2.0252234b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef389fc0cc4c88eadd20",
+    "url": "/commath-tictactoe/static/js/main.4b95fab1.chunk.js"
+  },
+  {
+    "revision": "d27f474918246e6e6a69",
+    "url": "/commath-tictactoe/static/js/runtime-main.9c7b4328.js"
+  }
+]);
